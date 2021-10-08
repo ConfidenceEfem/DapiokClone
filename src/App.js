808eaef796ca563c3996bd56e882d0cf5b1eb104
ConfidenceEfem <confidenceefem1@gmail.com>
2021-10-08 06:10:05 -0700

    DapiokClone

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import DapiokApp from './Dapiok Project/DapiokApp'
+
+const App = () => {
+    return (
+        <div>
+            <DapiokApp/>
+        </div>
+    )
+}
+
+export default App
